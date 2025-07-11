5024cdfce76a2042afdd2ac75d847857a7f4750f
Samaksh Gautam <151767180+samaksh-123@users.noreply.github.com>
2025-07-11 14:30:41 +0530

    Update Register.jsx

--- IT frontend/src/pages/Register.jsx
+++ IT frontend/src/pages/Register.jsx
@@ -275,7 +275,7 @@ const Register = () => {
     }
 
     try {
-      await axios.post('http://localhost:5000/api/auth/register', {
+      await axios.post('https://asset-main-backend.onrender.com/api/auth/register', {
         name: form.name,
         email: form.email,
         password: form.password
